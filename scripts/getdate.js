ff91cb87f905d4af6f63fa44bc403020d7fdf90d
Rosterrr <rodrigochanta331@gmail.com>
2026-03-07 08:20:45 -0600

    Delete scripts directory

--- scripts/getdate.js
+++
@@ -1,7 +0,0 @@
-// scripts/getdates.js
-
-const yearSpan = document.querySelector("#currentyear");
-yearSpan.textContent = new Date().getFullYear();
-
-const lastModifiedPara = document.querySelector("#lastModified");
-lastModifiedPara.innerHTML = `Last Modification: ${document.lastModified}`;
